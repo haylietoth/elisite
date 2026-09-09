fcf217c464c91c6f7c8c6e77d62994f959f48df5
hayliet <hayliet@andrew.cmu.edu>
2026-09-09 12:02:37 -0400

    fix to drawing image load issue

--- public/scripts/custom.js
+++ public/scripts/custom.js
@@ -50,7 +50,9 @@ jQuery (document).ready(function(){
   const drawings = Array.from(document.querySelectorAll(".drawing"));
 
   if (window.location.pathname === "/drawings" && drawings.length) {
-    applyAbsoluteLayout(drawings);
+    window.addEventListener('load', function () {
+      applyAbsoluteLayout(drawings);
+    })
   }
 
   function applyAbsoluteLayout(cards) {
@@ -234,92 +236,92 @@ jQuery (document).ready(function(){
     }
   });
 
-    /*info section animation*/
-    $(function(){
-        $('#info').click(function() {
-            if($('#site-footer').hasClass('closed')) {
-                $('.navbar-collapse').removeClass('in');
-                $('#site-footer').animate({'top': '80px'}, 1000);
-                    $(this).delay(1000)
-                           .queue(function () {
-                                $(this).addClass('material-icons').text('arrow_downward');
-                                $(this).dequeue();
-                           });
-                    $('#site-footer').removeClass('closed');
-            }
-            else {
-                $('#site-footer').addClass('closed');
-                $(this).removeClass('material-icons').text('Info');
-                $('#site-footer').animate({'top': '1500px'}, 1000);
-                $('.navbar-collapse').removeClass('in');
-            }
-        });
-    });
-    /*page fade-in and fade-out */
-    $('body').css('display','none');
-    $('body').fadeIn(500);
-
-    // $(document).on("click", "a", function () {
-    //     var newUrl = $(this).attr("href");
-    //     if (!newUrl || newUrl[0] === "#") {
-    //         location.hash = newUrl;
-    //         return;
-    //     }
-    //       $("html").fadeOut(function () {
-    //           location = newUrl;
-    //       });
-    //     return false;
-    // });
-    //random quote
-    (function() {
-      var quote = document.getElementsByClassName("quotes");
-      // Define images
-      var getQuote,totalQuotes;
-      var quotes = [];
-
-      for (i=0;i<quote.length;i++) {
-        var q = quote[i].innerText;
-        quotes.push(q);
-      }
-      // Counts total quotes
-      totalQuotes = quotes.length;
-
-      // Get Quote
-      getQuote = function() {
-        var activeQuotes;
-        // Gets random image from array
-        activeQuotes = quotes[Math.floor(Math.random() * totalQuotes)];
-        // Changes background of div
-        return $('.home-content').text(activeQuotes);
-      };
-
-      getQuote();
-    }).call(this);
-
-    //end random text//
-
-    ////filter////
-    $(document).ready(function(){
-      $("#write").click(function(){
-        $(".Designer").hide();
-        $(".Writer").show();
+  /*info section animation*/
+  $(function(){
+      $('#info').click(function() {
+          if($('#site-footer').hasClass('closed')) {
+              $('.navbar-collapse').removeClass('in');
+              $('#site-footer').animate({'top': '80px'}, 1000);
+                  $(this).delay(1000)
+                          .queue(function () {
+                              $(this).addClass('material-icons').text('arrow_downward');
+                              $(this).dequeue();
+                          });
+                  $('#site-footer').removeClass('closed');
+          }
+          else {
+              $('#site-footer').addClass('closed');
+              $(this).removeClass('material-icons').text('Info');
+              $('#site-footer').animate({'top': '1500px'}, 1000);
+              $('.navbar-collapse').removeClass('in');
+          }
       });
-      $("#design").click(function(){
-        $(".Writer").hide();
-        $(".Designer").show();
-      });
-    });
+  });
+  /*page fade-in and fade-out */
+  $('body').css('display','none');
+  $('body').fadeIn(500);
 
-    $(document).ready(function(){
+  // $(document).on("click", "a", function () {
+  //     var newUrl = $(this).attr("href");
+  //     if (!newUrl || newUrl[0] === "#") {
+  //         location.hash = newUrl;
+  //         return;
+  //     }
+  //       $("html").fadeOut(function () {
+  //           location = newUrl;
+  //       });
+  //     return false;
+  // });
+  //random quote
+  (function() {
+    var quote = document.getElementsByClassName("quotes");
+    // Define images
+    var getQuote,totalQuotes;
+    var quotes = [];
+
+    for (i=0;i<quote.length;i++) {
+      var q = quote[i].innerText;
+      quotes.push(q);
+    }
+    // Counts total quotes
+    totalQuotes = quotes.length;
+
+    // Get Quote
+    getQuote = function() {
+      var activeQuotes;
+      // Gets random image from array
+      activeQuotes = quotes[Math.floor(Math.random() * totalQuotes)];
+      // Changes background of div
+      return $('.home-content').text(activeQuotes);
+    };
+
+    getQuote();
+  }).call(this);
+
+  //end random text//
+
+  ////filter////
+  $(document).ready(function(){
+    $("#write").click(function(){
+      $(".Designer").hide();
+      $(".Writer").show();
+    });
+    $("#design").click(function(){
+      $(".Writer").hide();
+      $(".Designer").show();
+    });
+  });
+
+  $(document).ready(function(){
+    $(".long").hide();
+    $("#sh").click(function(){
       $(".long").hide();
-      $("#sh").click(function(){
-        $(".long").hide();
-        $(".short").show();
-      });
-      $("#lg").click(function(){
-        $(".short").hide();
-        $(".long").show();
-      });
+      $(".short").show();
     });
-    ////end filter//
+    $("#lg").click(function(){
+      $(".short").hide();
+      $(".long").show();
+    });
+  });
+  ////end filter//
 });
